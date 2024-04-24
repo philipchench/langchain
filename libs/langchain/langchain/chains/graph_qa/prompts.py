4570a4be6acdd5762c8139da8fd9a4f3f76baaed
Philip Chen <philipandrewchen@gmail.com>
2024-04-24 15:40:03 -0600

    modified select sparql prompt a bit

--- libs/langchain/langchain/chains/graph_qa/prompts.py
+++ libs/langchain/langchain/chains/graph_qa/prompts.py
@@ -135,16 +135,39 @@ SPARQL_INTENT_PROMPT = PromptTemplate(
     input_variables=["prompt"], template=SPARQL_INTENT_TEMPLATE
 )
 
+# SPARQL_GENERATION_SELECT_TEMPLATE = """Task: Generate a SPARQL SELECT statement for querying a graph database.
+# For instance, to find all email addresses of John Doe, the following query in backticks would be suitable:
+# ```
+# PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+# SELECT ?email
+# WHERE {{
+#     ?person foaf:name "John Doe" .
+#     ?person foaf:mbox ?email .
+# }}
+# ```
+# Instructions:
+# Use only the node types and properties provided in the schema.
+# Do not use any node types and properties that are not explicitly provided.
+# Include all necessary prefixes.
+# Schema:
+# {schema}
+# Note: Be as concise as possible.
+# Do not include any explanations or apologies in your responses.
+# Do not respond to any questions that ask for anything else than for you to construct a SPARQL query.
+# Do not include any text except the SPARQL query generated.
+
+# The question is:
+# {prompt}"""
+
 SPARQL_GENERATION_SELECT_TEMPLATE = """Task: Generate a SPARQL SELECT statement for querying a graph database.
-For instance, to find all email addresses of John Doe, the following query in backticks would be suitable:
-```
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+For instance, to find all email addresses of John Doe, the following query would be suitable:
+
 SELECT ?email
 WHERE {{
     ?person foaf:name "John Doe" .
     ?person foaf:mbox ?email .
 }}
-```
+
 Instructions:
 Use only the node types and properties provided in the schema.
 Do not use any node types and properties that are not explicitly provided.
@@ -158,6 +181,7 @@ Do not include any text except the SPARQL query generated.
 
 The question is:
 {prompt}"""
+
 SPARQL_GENERATION_SELECT_PROMPT = PromptTemplate(
     input_variables=["schema", "prompt"], template=SPARQL_GENERATION_SELECT_TEMPLATE
 )
